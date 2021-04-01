aad45e506cdfe2132943d3ed601833e2f16e918b
Jake Peterson <jpeterson0514@gmail.com>
2021-04-01 18:38:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,9 +15,9 @@ function logWhisper (string){
 }
 
 function sayHiToGrandma(string){
-  if(string === uppercase){
+  if(string === string.toUpperCase()){
     return "YES INDEED!"
-  } else if (string === lowercase){
+  } else if (string === string.toLowerCase()){
     return "I can't heare you!"
   } else if (string === "I love you, Grandma."){
     return "I love you, too."
